87f24f052a86f35a08c0add9a51e583207bb7533
neketli <neketli.cspl@gmail.com>
2024-11-29 17:50:21 +0300

    feat(frontend): add error page

---
+++ frontend/error.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="w-full h-screen flex items-center justify-center">
+        <ElResult
+            :title="$t('error.page.title')"
+            :sub-title="$t('error.page.not_found')"
+            icon="error"
+        >
+            <template #extra>
+                <ElButton
+                    class="ml-2"
+                    @click="handleError"
+                >
+                    {{ $t('error.page.back') }}
+                </ElButton>
+            </template>
+        </ElResult>
+    </div>
+</template>
+
+<script setup lang="ts">
+const { t } = useI18n()
+
+useHead({
+    title: t('common.error'),
+})
+
+const handleError = () => clearError({ redirect: '/' })
+</script>
