015c62fe180dd73f8f088e416ebbe1c7585c9c1e
neketli <neketli.cspl@gmail.com>
2025-03-02 21:41:30 +0300

    feat(landing): add mobile menu

--- landing/components/AppHeader.vue
+++ landing/components/AppHeader.vue
@@ -4,7 +4,7 @@
             class="max-w-7xl mx-auto px-4 sm:px-6 xl:px-8"
             aria-label="Top"
         >
-            <div class="w-full py-6 flex items-center justify-between border-b border-gray-200 xl:border-none">
+            <div class="w-full py-6 flex items-center justify-between  border-gray-200">
                 <div class="flex items-center">
                     <a href="#">
                         <span class="flex gap-2 text-2xl text-gray-900 font-['Helvetica']">
@@ -18,7 +18,7 @@
                         </span>
 
                     </a>
-                    <div class="hidden ml-10 space-x-8 xl:block">
+                    <div class="hidden ml-10 space-x-8 lg:block">
                         <a
                             v-for="link in navigation"
                             :key="link.name"
@@ -30,30 +30,9 @@
                     </div>
                 </div>
 
-                <div class="hidden ml-auto xl:flex gap-4 ">
-                    <button
-                        :class="{
-                            'text-gray-600 cursor-default': $i18n.locale === 'ru',
-                            'text-primary-600 transition hover:text-primary-700 cursor-pointer': $i18n.locale !== 'ru',
-                        }"
-                        @click="setLocale('ru')"
-                    >
-                        {{ $t('header.ru') }}
-                    </button>
-                    <span>
-                        |
-                    </span>
-                    <button
-                        :class="{
-                            'text-gray-600 cursor-default': $i18n.locale === 'en',
-                            'text-primary-600 transition hover:text-primary-700 cursor-pointer': $i18n.locale !== 'en',
-                        }"
-                        @click="setLocale('en')"
-                    >
-                        {{ $t('header.en') }}
-                    </button>
-                </div>
-                <div class="ml-4 space-x-4">
+                <LangSwitch class="hidden ml-auto lg:flex" />
+
+                <div class="hidden lg:block ml-4 space-x-4">
                     <a
                         href="https://projectly.ru/my/auth/login"
                         class="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-primary-600 hover:bg-gray-50"
@@ -62,54 +41,74 @@
                     </a>
                     <a
                         href="https://projectly.ru/my/auth/register"
-
                         class="inline-block bg-primary-600 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-primary-700"
                     >
                         {{ $t('header.register') }}
                     </a>
                 </div>
-            </div>
 
-            <div class="py-4 flex flex-wrap justify-center gap-4 xl:hidden">
                 <button
-                    :class="{
-                        'text-gray-600 cursor-default': $i18n.locale === 'ru',
-                        'text-primary-600 transition hover:text-primary-700 cursor-pointer': $i18n.locale !== 'ru',
-                    }"
-                    @click="setLocale('ru')"
+                    class="lg:hidden inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-500"
+                    @click="menu = !menu"
                 >
-                    {{ $t('header.ru') }}
-                </button>
-                <span>
-                    |
-                </span>
-                <button
-                    :class="{
-                        'text-gray-600 cursor-default': $i18n.locale === 'en',
-                        'text-primary-600 transition hover:text-primary-700 cursor-pointer': $i18n.locale !== 'en',
-                    }"
-                    @click="setLocale('en')"
-                >
-                    {{ $t('header.en') }}
+                    <Icon :name="menu ? 'mdi:close' : 'mdi:menu' " />
                 </button>
             </div>
 
-            <div class="py-4 flex flex-wrap justify-center space-x-6 xl:hidden">
-                <a
-                    v-for="link in navigation"
-                    :key="link.name"
-                    :href="link.href"
-                    class="text-base font-medium text-gray-600 hover:text-gray-900"
+            <Transition
+                enter-active-class="transition ease-out duration-100"
+                enter-from-class="transform opacity-0 scale-95 -translate-y-[80px]"
+                enter-to-class="transform opacity-100 scale-100"
+                leave-active-class="transition ease-in duration-75"
+                leave-from-class="transform opacity-100 scale-100"
+                leave-to-class="transform opacity-0 scale-95 -translate-y-[80px]"
+            >
+                <div
+                    v-show="menu"
+                    class="lg:hidden absolute top-[80px] right-0 w-full bg-white border border-gray-200 rounded-md shadow-md"
                 >
-                    {{ link.name }}
-                </a>
-            </div>
+                    <div class="py-4 flex flex-wrap justify-center gap-4">
+                        <a
+                            href="https://projectly.ru/my/auth/login"
+                            class="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-primary-600 hover:bg-gray-50"
+                        >
+                            {{ $t('header.login') }}
+                        </a>
+                        <a
+                            href="https://projectly.ru/my/auth/register"
+                            class="inline-block bg-primary-600 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-primary-700"
+                        >
+                            {{ $t('header.register') }}
+                        </a>
+                    </div>
+
+                    <LangSwitch />
+
+                    <div
+                        class="py-4 flex flex-wrap justify-center space-x-6"
+                        @click="menu = false"
+                    >
+                        <a
+                            v-for="link in navigation"
+                            :key="link.name"
+                            :href="link.href"
+                            class="text-base font-medium text-gray-600 hover:text-gray-900"
+                        >
+                            {{ link.name }}
+                        </a>
+                    </div>
+                </div>
+            </Transition>
         </nav>
     </header>
 </template>
 
 <script setup>
-const { t, setLocale } = useI18n()
+import LangSwitch from './ui/LangSwitch.vue'
+
+const { t } = useI18n()
+
+const menu = ref(false)
 
 const navigation = computed(() => [
     { name: t('header.nav.features'), href: '#features' },
